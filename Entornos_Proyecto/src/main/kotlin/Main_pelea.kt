420e5cbd2ec66ff314a6e6973e13abf9b45f4a04
[Marina] <[marinalaguna2004@gmail.com]>
2023-03-24 11:12:24 +0100

    Update Main_pelea.kt

--- Entornos_Proyecto/src/main/kotlin/Main_pelea.kt
+++ Entornos_Proyecto/src/main/kotlin/Main_pelea.kt
@@ -1,9 +1,6 @@
 import Pelea.PeleaDAOImpl
 import Pelea.PeleaFichero
 
-val PeleaDAO = PeleaDAOImpl()
-val PeleaFichero = PeleaFichero()
-
 fun main() {
     val PeleaDAO = PeleaDAOImpl()
     val PeleaFichero = PeleaFichero()
