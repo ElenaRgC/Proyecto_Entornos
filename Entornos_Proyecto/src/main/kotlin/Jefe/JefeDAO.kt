ab07d3838d45ba3e65d88be9f74fa49f46bcb576
patrimj <patriimj1@gmail.com>
2023-03-24 13:16:17 +0100

    Creada función InsertarJefe()

--- Entornos_Proyecto/src/main/kotlin/Jefe/JefeDAO.kt
+++ Entornos_Proyecto/src/main/kotlin/Jefe/JefeDAO.kt
@@ -1,8 +1,10 @@
 package Jefe
 
+import Habilidad.Habilidad
 import Jefe.Jefe
 
 interface JefeDAO {
     fun todosLosJefes(): List<Jefe>
     fun insertarLista(c:ArrayList<Jefe>):ArrayList<Jefe>
+    fun insertarJefe(jefe: Jefe): Boolean
 }
--- Entornos_Proyecto/src/main/kotlin/Main_jefe.kt
+++ Entornos_Proyecto/src/main/kotlin/Main_jefe.kt
@@ -1,15 +1,22 @@
+import Habilidad.Habilidad
+import Jefe.Jefe
 import Jefe.JefeDAOImpl
 import Jefe.JefeFichero
 
-val JefeDAO = JefeDAOImpl()
-val JefeFichero = Jefe.JefeFichero()
+
 fun main() {
     val JefeDAO = JefeDAOImpl()
-    val JefeFichero = Jefe.JefeFichero()
+    val JefeFichero = JefeFichero()
 
     val jefe = JefeDAO.todosLosJefes()
     println("Lista de jefes:")
     jefe.forEach { println(it) }
     println(jefe)
 
+    val nuevoJefe = Jefe(13,"Ashava El Pestilente",7500,"Facil", "Utiliza su mano derecha para aplastar y causar daño de área")
+    if (JefeDAO.insertarJefe(nuevoJefe)) {
+        println("Se insertó correctamente el jefe ${nuevoJefe.nombreJ}")
+    } else {
+        println("No se pudo insertar el jefe ${nuevoJefe.nombreJ}")
+    }
 }
