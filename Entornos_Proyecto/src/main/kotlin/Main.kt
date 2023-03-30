e28134033be6973d176886146503d54fca6ba8d4
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-03-30 13:33:20 +0200

    Separados menús en funciones

--- Entornos_Proyecto/src/main/kotlin/Main.kt
+++ Entornos_Proyecto/src/main/kotlin/Main.kt
@@ -36,11 +36,22 @@ fun main(args: Array<String>) {
         conexion.desconectar()
     }
 
-    println("¿Con qué tabla deseas trabajar?")
-    val tablaUsuario = readln().trim().lowercase()
+    do {
+        println("¿Con qué tabla deseas trabajar?")
+        var tablaUsuario = readln().trim().lowercase()
+        val objetoUsuario = menuTabla(tablaUsuario)
+
+        var opcionUsuario = mostrarMenuOpciones(tablaUsuario)
+
+        ejecutarOpcionMenu(tablaUsuario, opcionUsuario, objetoUsuario)
+    } while (opcionUsuario != "5")
+
+}
+
+fun menuTabla(nombreTabla: String): Any {
     var objetoUsuario = Any()
 
-    when (tablaUsuario) {
+    when (nombreTabla) {
         "habilidad" -> {
             objetoUsuario = HabilidadDAOImpl()
             var habilidad = objetoUsuario.todosLasHabilidades()
@@ -48,6 +59,7 @@ fun main(args: Array<String>) {
             habilidad.forEach { println(it) }
             println()
         }
+
         "jefe" -> {
             objetoUsuario = JefeDAOImpl()
             var jefe = objetoUsuario.todosLosJefes()
@@ -55,52 +67,73 @@ fun main(args: Array<String>) {
             jefe.forEach { println(it) }
             println()
         }
+
         "pelea" -> {
             objetoUsuario = PeleaDAOImpl()
             var pelea = objetoUsuario.todosLasPeleas()
             println("Lista de peleas:")
-            pelea.forEach {println(it)}
+            pelea.forEach { println(it) }
             println()
         }
+
         "personaje" -> {
             objetoUsuario = PersonajeDAOImpl()
             var personaje = objetoUsuario.todosLosPersonajes()
             println("Lista de personajes:")
-            personaje.forEach {println(it)}
+            personaje.forEach { println(it) }
             println()
         }
+
         else -> println("Seleccione una tabla de la lista anterior.")
     }
 
-    print("¿Qué quieres hacer con la tabla ${tablaUsuario.uppercase()}?")
+    return objetoUsuario
+
+}
+
+fun mostrarMenuOpciones(nombreTabla: String): String {
+    print("¿Qué quieres hacer con la tabla ${nombreTabla.uppercase()}?")
     println(" (Introduzca la palabra indicada en mayúsculas o su número correspondiente.)")
-    println("1. ANADIR un/a $tablaUsuario nuevo/a.")
+    println("1. ANADIR un/a $nombreTabla nuevo/a.")
     println("2. BORRAR una fila.")
     println("3. MODIFICAR un campo.")
     println("\n O también:")
-    println("CAMBIAR a otra tabla.")
-    println("SALIR del programa.")
+    println("4. CAMBIAR a otra tabla.")
+    println("5. SALIR del programa.")
 
-    var opcionUsuario = readln().trim().lowercase()
+    return readln().trim().lowercase()
+}
 
-    when (opcionUsuario) {
+fun ejecutarOpcionMenu(nombreTabla: String, eleccion: String, objeto: Any) {
+    when (eleccion) {
         "anadir", "1" -> {
+
         }
+
         "borrar", "2" -> {
-            println("Introduce el nombre del/de la $tablaUsuario que quieres borrar.")
+            println("Introduce el nombre del/de la $nombreTabla que quieres borrar.")
             var nombreElemento = readln().trim().lowercase()
-            if(objetoUsuario.borrarFila(nombreElemento)) {
-                println("$nombreElemento se ha borrado correctamente de la tabla $tablaUsuario.")
+            if (objeto.borrarFila(nombreElemento)) {
+                println("$nombreElemento se ha borrado correctamente de la tabla $nombreTabla.")
             } else {
-                println("No ha podido eliminarse $nombreElemento de la tabla $tablaUsuario.")
+                println("No ha podido eliminarse $nombreElemento de la tabla $nombreTabla.")
             }
         }
-        "modificar", "3" -> {}
-        "cambiar" -> {}
-        "salir" -> {}
+
+        "modificar", "3" -> {
+
+        }
+
+        "cambiar", "4" -> {
+
+        }
+
+        "salir", "5" -> {
+            println("Saliendo del programa.")
+        }
+
         else -> println("Seleccione una opción de la lista anterior.")
     }
-
 }
 
 
