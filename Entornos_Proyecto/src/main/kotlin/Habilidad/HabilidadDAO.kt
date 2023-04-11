ffb71ef9d6128625d18f653f8d18d587d2cdf770
[Marina] <[marinalaguna2004@gmail.com]>
2023-04-11 22:48:02 +0200

    eliminada carpeta de la rama de kdoc

--- Entornos_Proyecto/src/main/kotlin/Habilidad/HabilidadDAO.kt
+++
@@ -1,9 +0,0 @@
-package Habilidad
-
-interface HabilidadDAO {
-    fun todosLosCampos(): List<Habilidad>
-    fun insertarFila(habilidad: Habilidad): Boolean
-    fun recibirDescripcion(nombreHabilidad: String): String?
-    fun borrarFila(nombre: String): Boolean
-    fun modificarCampo(nombre: String, nombreCampo: String, nuevoValorCampo: String): Boolean
-}
--- Entornos_Proyecto/src/main/kotlin/Jefe/JefeDAO.kt
+++
@@ -1,9 +0,0 @@
-package Jefe
-
-interface JefeDAO {
-    fun todosLosCampos(): List<Jefe>
-    fun insertarFila(jefe: Jefe): Boolean
-    fun recibirDescripcion(nombreJefe: String): String?
-    fun borrarFila(nombre: String): Boolean
-    fun modificarCampo(nombre: String, nombreCampo: String, nuevoValorCampo: String): Boolean
-}
